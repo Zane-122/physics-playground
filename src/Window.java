00c591bcc6baf86764b52ef03d9e5607111d5842
Aryan Chaoji <spacerxmc1@gmail.com>
2026-05-20 07:32:07 -0400

    completed window

--- src/Window.java
+++ src/Window.java
@@ -4,20 +4,21 @@ import javax.swing.SwingUtilities;
 public class Window {
     private double width;
     private double height;
+    private String title;
 
-    public Window(double w, double h) {
+    public Window(double w, double h, String t) {
         width = w;
         height = h;
+        title = t;
     }
 
     public void display() {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("My Window");
+            JFrame frame = new JFrame(title);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize((int) width, (int) height);
             frame.setVisible(true);
         });
     }
 }
-
 
