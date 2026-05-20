5bf051990329fcef22efc0c8c876a64085cdc269
Zan e1 <zbeidas@icloud.com>
2026-05-20 08:01:01 -0400

    Created polygons

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("SSup, World!");
+        DrawingPanel drawingPanel = new DrawingPanel();
 
-        Window myWindow = new Window(400, 300, "Physics Playground");
+        Window myWindow = new Window(400, 300, "Physics Playground", drawingPanel);
+
+        drawingPanel.addObject(new Polygon(6, 100, new Util.Point(100, 100)));
         myWindow.display();
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -5,11 +5,13 @@ public class Window {
     private double width;
     private double height;
     private String title;
+    private DrawingPanel panel;
 
-    public Window(double w, double h, String t) {
+    public Window(double w, double h, String t, DrawingPanel panel) {
         width = w;
         height = h;
         title = t;
+        this.panel = panel;
     }
 
     public void display() {
@@ -18,6 +20,7 @@ public class Window {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize((int) width, (int) height);
             frame.setVisible(true);
+            frame.add(panel);
         });
     }
 }
