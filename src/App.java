7f4b8ba3276af22b62dc45f296180b3ca7c2b69e
Aryan Chaoji <spacerxmc1@gmail.com>
2026-05-20 07:34:05 -0400

    passed in window name

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("SSup, World!");
 
-        Window myWindow = new Window(400, 300);
+        Window myWindow = new Window(400, 300, "Physics Playground");
         myWindow.display();
     }
 }
