3d578d3a7cec465548704180a2d4b2bfd87d8852
Aryan Chaoji <spacerxmc1@gmail.com>
2026-05-21 08:04:55 -0400

    made gravity phsyics

--- src/Drawable.java
+++ src/Drawable.java
@@ -1,6 +1,7 @@
-
 import java.awt.Graphics;
 
 public interface Drawable {
     void draw(Graphics g);
+    void setPosition(Util.Point p);
+    Util.Point getPosition();
 }
--- src/Util.java
+++ src/Util.java
@@ -1,3 +1,4 @@
 public class Util {
     public static record Point(double x, double y){};
+    public static record Vector(double x, double y){};
 }
