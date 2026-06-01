d758cc3e5645af254425091c6c91e154b3f1a0c2
Zan e1 <zbeidas@icloud.com>
2026-06-01 08:08:24 -0400

    Added freeze component to work

--- src/Component.java
+++ src/Component.java
@@ -1,3 +1,7 @@
 public abstract class Component {
+    public void initialize(PhysicsObject object) {
+        // do nothing by default
+    }
+
     public abstract void update(PhysicsObject object, Simulation sim);
 }
